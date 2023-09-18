0ffcafcc4c0c12d82286f62ecc270560ebab8272
mohit rajput <mouryamohitsingh@gmail.com>
2023-09-18 14:24:29 +0530

    File add

--- src/travewave/Main.java
+++ src/travewave/Main.java
@@ -8,13 +8,14 @@ package travewave;
  *
  * @author Dell
  */
-public class TraveWave {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("helloooooooo");
+            
     }
     
 }
